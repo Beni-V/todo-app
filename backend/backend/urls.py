212e518632523f59d18f4c075b53d19a388b3803
Beni-V <binitto10@gmail.com>
2023-04-06 22:03:28 +0300

    add to urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.authtoken.views import ObtainAuthToken
 
+from core.views import TodoItemViewSet
 from users.views import UserRegistrationView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/login/", ObtainAuthToken.as_view()),
     path("api/register/", UserRegistrationView.as_view()),
+    path("api/todo_items/", TodoItemViewSet.as_view()),
 ]
