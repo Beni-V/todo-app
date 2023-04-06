2bede7e849281b41861cdfdcdc2ad7179e2d601d
Beni-V <binitto10@gmail.com>
2023-04-06 22:00:39 +0300

    docs and permission classes

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from core.serializers import TodoItemSerializer
 
 
 class TodoItemViewSet(viewsets.ModelViewSet):
+    """View-set for `TodoItem`s"""
+
     serializer_class = TodoItemSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        """Return the todo items for the current user"""
         return self.request.user.todo_items.all()
