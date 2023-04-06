764573972699a8e0a177de28002ce798a4094058
Beni-V <binitto10@gmail.com>
2023-04-06 18:56:44 +0300

    docs

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,13 +5,23 @@ from users.models import User
 
 
 class UserRegistrationSerializer(ModelSerializer):
+    """Serializer for user registration request"""
     class Meta:
         model = User
         fields = ("username", "password")
 
     def create(self, validated_data):
+        """
+        Create a new user with django `UserManager` and return it
+
+        Django built in manager hashes the password and normalizes the inputs
+        """
         return User.objects.create_user(**validated_data)
 
     def validate(self, attrs):
+        """Validate user input for registration and return it"""
+
+        # validate password with django provided function
         validate_password(attrs.get("password"))
+
         return super().validate(attrs)
