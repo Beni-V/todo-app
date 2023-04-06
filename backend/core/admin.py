29876d7a6630a3c5f58db9d934f2086aa2f43fd6
Beni-V <binitto10@gmail.com>
2023-04-06 21:57:39 +0300

    add todo item and user to admin panel

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from core.models import TodoItem
+
+
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "completed", "user")
