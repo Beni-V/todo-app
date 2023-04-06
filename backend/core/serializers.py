32875cd2d2f903cab96cb89a68b14e3328fd9d1d
Beni-V <binitto10@gmail.com>
2023-04-06 22:31:51 +0300

    edit serializer fields

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -5,4 +5,4 @@ from users import serializers
 class TodoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoItem
-        fields = "__all__"
+        fields = "title, description, completed"
