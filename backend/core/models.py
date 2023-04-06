32b1a94e0e2582123cea3da9cd1a9b88067b87ed
Beni-V <binitto10@gmail.com>
2023-04-06 22:52:19 +0300

    add order id for user todo items list

--- backend/core/models.py
+++ backend/core/models.py
@@ -22,12 +22,22 @@ class TodoItem(models.Model):
 
     user : User
         The user for which this todo item belongs.
+
+    order_id : int
+        The order of the todo item in the list of `User`s todo items.
     """
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200, blank=True, null=True)
     completed = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todo_items")
+    order_id = models.PositiveIntegerField(blank=True, null=True)
+
+    class Meta:
+        """Metaclass for TodoItem model"""
+
+        # order_id should be unique for each user
+        unique_together = ("user", "order_id")
 
     def __str__(self):
         return self.title
