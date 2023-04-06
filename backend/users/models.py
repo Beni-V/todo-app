568b715029cecbcfb472bb98bc2a6116b344e37a
Beni-V <binitto10@gmail.com>
2023-04-06 16:46:54 +0300

    change the approach and user `AbstractUser` provided by django

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,27 +1,9 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.validators import UnicodeUsernameValidator
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
     """
     Represents the user for the application
 
-    Inherits from AbstractBaseUser which is a django class that provides the basic functionality for a user
-    like password validation and last login time.
-
-    Attributes:
-    -----------
-    username: str
-        The username of the user
-    username_validator: RegexValidator
-        The validator for the username provided by django
+    Inherits from AbstractUser which is a django class that provides the basic functionality for a user
     """
-
-    username_validator = UnicodeUsernameValidator()
-
-    username = models.CharField(
-        max_length=150, unique=True, validators=[username_validator]
-    )
-
-    USERNAME_FIELD = "username"
