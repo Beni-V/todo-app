74e880166016a8a82f694193c22c4bae11bc7efc
Beni-V <binitto10@gmail.com>
2023-04-09 01:43:57 +0300

    remove this thing

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,21 +1,11 @@
 import '../styles/globals.css';
 import { AuthProvider } from '@/contexts/authContext';
-import { ChakraProvider, extendBaseTheme } from '@chakra-ui/react';
-
-import chakraTheme from '@chakra-ui/theme';
+import { ChakraProvider } from '@chakra-ui/react';
 import Layout from '@/components/Layout';
 
-const { Button, Alert } = chakraTheme.components;
-
-const theme = extendBaseTheme({
-  components: {
-    Button,
-    Alert
-  }
-});
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <AuthProvider>
         <Layout>
           <Component {...pageProps} />
