bb141e0d3654f7059cd0865809a5f3029a30e34f
Beni-V <binitto10@gmail.com>
2023-04-09 23:00:48 +0300

    update useTodos hook. (temporary)

--- frontend/hooks/useTodos.js
+++ frontend/hooks/useTodos.js
@@ -2,34 +2,25 @@ import { useState, useEffect } from 'react';
 
 const useTodos = () => {
   const [todos, setTodos] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchTodos = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch('http://localhost:8000/api/todo_items/', {
-          method: 'GET',
-          headers: { Authorization: localStorage.Authorization }
-        });
-        let data = await response.json();
-        data = data.map((item) => ({
-          ...item,
-          orderId: item.orderId.toString()
-        }));
-        setTodos(data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
+      const response = await fetch('http://localhost:8000/api/todo_items/', {
+        method: 'GET',
+        headers: { Authorization: localStorage.Authorization }
+      });
+      let data = await response.json();
+      data = data.map((item) => ({
+        ...item,
+        orderId: item.orderId.toString()
+      }));
+      setTodos(data);
     };
 
     fetchTodos();
   }, []);
 
-  return { todos, setTodos, loading, error };
+  return { todos, setTodos };
 };
 
 export default useTodos;
